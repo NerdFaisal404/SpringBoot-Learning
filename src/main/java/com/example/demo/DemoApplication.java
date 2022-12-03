904ff3cbaca71ecb264184da1df5966da42b3b31
faisalahmed <faisal.ahmedbcse@gmail.com>
2022-12-03 14:47:44 +0600

    Add Custom Json Response Class

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Objects;
 
 @SpringBootApplication
 @RestController
@@ -23,6 +24,37 @@ public class DemoApplication {
 	@GetMapping("/thanks")
 	public List<String> thanksShimulBhai(){
 		return List.of("Jajak Allah","Shimul Bhai");
+	}
+
+	@GetMapping("/greet")
+	public GreetResponse greeting(){
+		return new GreetResponse("Hello");
+	}
+
+
+	class GreetResponse{
+		private final String greet;
+
+		public GreetResponse(String greet) {
+			this.greet = greet;
+		}
+
+		public String getGreet() {
+			return greet;
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (this == o) return true;
+			if (o == null || getClass() != o.getClass()) return false;
+			GreetResponse that = (GreetResponse) o;
+			return greet.equals(that.greet);
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(greet);
+		}
 	}
 
 }
